622e9cea2dab28bfce9d01bc037b138a1df59fa3
Gian8889 <jorgehc318@gmail.com>
2026-05-22 22:12:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ if sesion == "Sesion 1":
   st.image("pythonlogo.png")
   
 elif sesion == "Sesion 2":
-  st.write("Bienvenido a la sesion 2")  
+  st.write("Bienvenido a la sesion 2")
+
+  precio = st.number.input("Ingrese el precio del producto", min_value=0, max_value=5000, value=1200)
+  descuento = st.number.input("Ingrese el descuento del producto del 0 al 100%", min_value=0, max_value=100)
+
+  precio_final_producto = precio - (precio*descuento/100)
+
+  st.write("El producto final del producto es: ", precio_final_producto)
 elif sesion == "Sesion 3":
   st.write("Bienvenido a la sesion 3")
 elif sesion == "Sesion 4":
