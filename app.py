ce1b2baab77d44d11cf104ef44a09fa0ebaee5fc
Gian8889 <jorgehc318@gmail.com>
2026-05-22 21:46:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en Python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por : Jorge Huaman")
+
+sesion = st.sidebar.selectbox("Seleccione una sesion", ["Sesion 1","Sesion 2","Sesion 3","Sesion 4"])
