580084d629b3d8d1392d29f36e1d36ad3540fb54
Gian8889 <jorgehc318@gmail.com>
2026-05-22 22:07:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por : Jorge Huaman")
 
+st.sidebar.image("dmclogo.png")
+
 sesion = st.sidebar.selectbox("Seleccione una sesion", ["Sesion 1","Sesion 2","Sesion 3","Sesion 4"])
 
 if sesion == "Sesion 1":
   st.write("Bienvenido a la sesion 1")
   st.image("pythonlogo.png")
+  
 elif sesion == "Sesion 2":
   st.write("Bienvenido a la sesion 2")  
-if sesion == "Sesion 3":
+elif sesion == "Sesion 3":
   st.write("Bienvenido a la sesion 3")
-if sesion == "Sesion 4":
+elif sesion == "Sesion 4":
   st.write("Bienvenido a la sesion 4")
